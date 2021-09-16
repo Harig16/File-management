de6fff1ad391f14faa241ce673c29d251b4f5bed
Harig16 <95hari.gopal@gmail.com>
2021-09-16 06:14:16 +0530

    Create Users signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :documents, only: [:index, :new, :create, :destroy]
   root to: "documents#index"
+
+  get 'signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
